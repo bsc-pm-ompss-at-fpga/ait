f7903190300318caf27443eee08438245bb2f1a5
Miquel Vidal Piñol <miquel.vidal@bsc.es>
2023-03-27 10:41:16 +0200

    Fix remaining staticRegSlices scripts

--- ait/backend/xilinx/board/alveo_u200/staticRegSlices.tcl
+++ ait/backend/xilinx/board/alveo_u200/staticRegSlices.tcl
@@ -1,6 +1,6 @@
 proc AIT::create_DDR_reg_slice {ddrName ddrPort masterSlr migSlr} {
     set ddrRegSlice [ create_bd_cell -type ip -vlnv xilinx.com:ip:axi_register_slice:2.1 \
-        bridge_to_host/DDR/${ddrName}_${ddrPort}_slr_static_${masterSlr}_${migSlr} ]
+        bridge_to_host/memory/${ddrName}_${ddrPort}_slr_static_${masterSlr}_${migSlr} ]
     set_property -dict [ list \
         CONFIG.NUM_SLR_CROSSINGS {0} \
         CONFIG.REG_AR {15} \
@@ -12,11 +12,11 @@ proc AIT::create_DDR_reg_slice {ddrName ddrPort masterSlr migSlr} {
         ] $ddrRegSlice
 
     # Delete interface nets
-    delete_bd_objs [get_bd_intf_nets -of_objects [get_bd_intf_pins bridge_to_host/DDR/${ddrName}/C0_DDR4_${ddrPort}]]
-    connect_bd_intf_net [get_bd_intf_pins $ddrRegSlice/M_AXI] [get_bd_intf_pins bridge_to_host/DDR/${ddrName}/C0_DDR4_${ddrPort}]
-    connect_bd_intf_net [get_bd_intf_pins $ddrRegSlice/S_AXI] [get_bd_intf_pins bridge_to_host/DDR/${ddrName}_${ddrPort}]
+    delete_bd_objs [get_bd_intf_nets -of_objects [get_bd_intf_pins bridge_to_host/memory/${ddrName}/C0_DDR4_${ddrPort}]]
+    connect_bd_intf_net [get_bd_intf_pins $ddrRegSlice/M_AXI] [get_bd_intf_pins bridge_to_host/memory/${ddrName}/C0_DDR4_${ddrPort}]
+    connect_bd_intf_net [get_bd_intf_pins $ddrRegSlice/S_AXI] [get_bd_intf_pins bridge_to_host/memory/${ddrName}_${ddrPort}]
 
-    connect_bd_net [get_bd_pins $ddrRegSlice/aclk] [get_bd_pins bridge_to_host/DDR/${ddrName}/c0_ddr4_ui_clk]
+    connect_bd_net [get_bd_pins $ddrRegSlice/aclk] [get_bd_pins bridge_to_host/memory/${ddrName}/c0_ddr4_ui_clk]
     connect_bd_net [get_bd_pins $ddrRegSlice/aresetn] [get_bd_pins /bridge_to_host/memory/${ddrName}_procSysRst/peripheral_aresetn]
 }
 
--- ait/backend/xilinx/board/euroexa_maxilink_quad/staticRegSlices.tcl
+++ ait/backend/xilinx/board/euroexa_maxilink_quad/staticRegSlices.tcl
@@ -1,6 +1,6 @@
 proc AIT::create_DDR_reg_slice {ddrName ddrPort masterSlr migSlr ctrl} {
     set ddrRegSlice [ create_bd_cell -type ip -vlnv xilinx.com:ip:axi_register_slice:2.1 \
-        bridge_to_host/DDR/${ddrName}/DDR_${ddrPort}_slr_static_${masterSlr}_${migSlr} ]
+        bridge_to_host/memory/${ddrName}/DDR_${ddrPort}_slr_static_${masterSlr}_${migSlr} ]
     set_property -dict [ list \
         CONFIG.NUM_SLR_CROSSINGS {0} \
         CONFIG.REG_AR {15} \
@@ -12,16 +12,16 @@ proc AIT::create_DDR_reg_slice {ddrName ddrPort masterSlr migSlr ctrl} {
         ] $ddrRegSlice
 
     # Delete interface nets
-    delete_bd_objs [get_bd_intf_nets -of_objects [get_bd_intf_pins bridge_to_host/DDR/${ddrName}/DDR/C0_DDR4_${ddrPort}]]
-    connect_bd_intf_net [get_bd_intf_pins $ddrRegSlice/M_AXI] [get_bd_intf_pins bridge_to_host/DDR/${ddrName}/DDR/C0_DDR4_${ddrPort}]
+    delete_bd_objs [get_bd_intf_nets -of_objects [get_bd_intf_pins bridge_to_host/memory/${ddrName}/DDR/C0_DDR4_${ddrPort}]]
+    connect_bd_intf_net [get_bd_intf_pins $ddrRegSlice/M_AXI] [get_bd_intf_pins bridge_to_host/memory/${ddrName}/DDR/C0_DDR4_${ddrPort}]
     if { $ctrl } {
-        connect_bd_intf_net [get_bd_intf_pins $ddrRegSlice/S_AXI] [get_bd_intf_pins bridge_to_host/DDR/${ddrName}/DDR_S_AXI_CTRL]
+        connect_bd_intf_net [get_bd_intf_pins $ddrRegSlice/S_AXI] [get_bd_intf_pins bridge_to_host/memory/${ddrName}/DDR_S_AXI_CTRL]
     } else {
-        connect_bd_intf_net [get_bd_intf_pins $ddrRegSlice/S_AXI] [get_bd_intf_pins bridge_to_host/DDR/${ddrName}/DDR_S_AXI_regslice/M_AXI]
+        connect_bd_intf_net [get_bd_intf_pins $ddrRegSlice/S_AXI] [get_bd_intf_pins bridge_to_host/memory/${ddrName}/DDR_S_AXI_regslice/M_AXI]
     }
 
-    connect_bd_net [get_bd_pins $ddrRegSlice/aclk] [get_bd_pins bridge_to_host/DDR/${ddrName}/DDR/c0_ddr4_ui_clk]
-    connect_bd_net [get_bd_pins $ddrRegSlice/aresetn] [get_bd_pins bridge_to_host/DDR/${ddrName}/DDR_proc_sys_reset/peripheral_aresetn]
+    connect_bd_net [get_bd_pins $ddrRegSlice/aclk] [get_bd_pins bridge_to_host/memory/${ddrName}/DDR/c0_ddr4_ui_clk]
+    connect_bd_net [get_bd_pins $ddrRegSlice/aresetn] [get_bd_pins bridge_to_host/memory/${ddrName}/DDR_proc_sys_reset/peripheral_aresetn]
 }
 
 proc AIT::static_logic_register_slices {} {
