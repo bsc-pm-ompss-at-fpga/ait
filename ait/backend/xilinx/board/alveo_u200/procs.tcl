127efda83c79d74a6b0951e6655d805ef6d22748
Miquel Vidal i Pinyol <miquel.vidal@bsc.es>
2023-11-16 16:51:20 +0100

    Added power_monitor support for alveo_u200 board

--- ait/backend/xilinx/board/alveo_u200/procs.tcl
+++ ait/backend/xilinx/board/alveo_u200/procs.tcl
@@ -26,5 +26,50 @@ namespace eval AIT {
 
             save_bd_design
         }
+
+        proc add_power_monitor {} {
+            # Add CMS subsystem and its system reset
+            create_bd_cell -type ip -vlnv xilinx.com:ip:cms_subsystem cms_subsystem
+            create_bd_cell -type ip -vlnv xilinx.com:ip:proc_sys_reset power_monitor_sys_rst
+
+            # Add an additional 50MHz clock for the CMS subsystem
+            set num_out_clocks [get_property CONFIG.NUM_OUT_CLKS [get_bd_cells clock_generator]]
+            incr num_out_clocks
+            set_property -dict [list \
+              CONFIG.NUM_OUT_CLKS $num_out_clocks \
+              CONFIG.CLKOUT${num_out_clocks}_USED {true} \
+              CONFIG.CLKOUT${num_out_clocks}_REQUESTED_OUT_FREQ {50} \
+              CONFIG.CLK_OUT${num_out_clocks}_PORT {power_monitor_clk}
+            ] [get_bd_cells clock_generator]
+
+            # Connect CMS clock and reset
+            connect_clock [get_bd_pins power_monitor_sys_rst/slowest_sync_clk] [get_bd_pins clock_generator/power_monitor_clk]
+            connect_reset [get_bd_pins power_monitor_sys_rst/ext_reset_in] [get_bd_pins processor_system_reset/ext_reset_in]
+
+            # Add and connect external ports
+            set satellite_uart [create_bd_intf_port -mode Master -vlnv xilinx.com:interface:uart_rtl:1.0 satellite_uart]
+            set satellite_gpio [create_bd_port -dir I -from 3 -to 0 -type intr satellite_gpio]
+            set_property CONFIG.SENSITIVITY {EDGE_RISING} $satellite_gpio
+            connect_bd_intf_net $satellite_uart [get_bd_intf_pins cms_subsystem/satellite_uart]
+            connect_bd_net $satellite_gpio [get_bd_pins cms_subsystem/satellite_gpio]
+            for {set i 0} {$i < 2} {incr i} {
+                # Output ports
+                set qsfp_lpmode [create_bd_port -dir O -from 0 -to 0 qsfp${i}_lpmode]
+                set qsfp_modsel_l [create_bd_port -dir O -from 0 -to 0 qsfp${i}_modsel_l]
+                set qsfp_reset_l [create_bd_port -dir O -from 0 -to 0 qsfp${i}_reset_l]
+                connect_bd_net [get_bd_pins cms_subsystem/qsfp${i}_lpmode] $qsfp_lpmode
+                connect_bd_net [get_bd_pins cms_subsystem/qsfp${i}_modsel_l] $qsfp_modsel_l
+                connect_bd_net [get_bd_pins cms_subsystem/qsfp${i}_reset_l] $qsfp_reset_l
+
+                # Input ports
+                set qsfp_int_l [create_bd_port -dir I -from 0 -to 0 qsfp${i}_int_l]
+                set qsfp_modprs_l [create_bd_port -dir I -from 0 -to 0 qsfp${i}_modprs_l]
+                connect_bd_net $qsfp_int_l [get_bd_pins cms_subsystem/qsfp${i}_int_l]
+                connect_bd_net $qsfp_modprs_l [get_bd_pins cms_subsystem/qsfp${i}_modprs_l]
+            }
+
+            # Connect CMS to the M_AXI interconnect
+            connect_to_axi_intf [get_bd_intf_pins cms_subsystem/s_axi_ctrl] M "" [get_bd_pins clock_generator/power_monitor_clk] [get_bd_pins power_monitor_sys_rst/peripheral_aresetn]
+        }
     }
 }
