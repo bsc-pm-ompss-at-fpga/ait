972fd72cb4e39c51f63bd876193cb001347e0e4b
Miquel Vidal i Pinyol <miquel.vidal@bsc.es>
2023-11-21 15:52:38 +0100

    Added thermal_monitor support for alveo_u280 board

--- ait/backend/xilinx/board/alveo_u280/procs.tcl
+++ ait/backend/xilinx/board/alveo_u280/procs.tcl
@@ -48,5 +48,28 @@ namespace eval AIT {
             # Connect CMS to the M_AXI interconnect
             connect_to_axi_intf [get_bd_intf_pins cms_subsystem/s_axi_ctrl] M "" [get_bd_pins clock_generator/power_monitor_clk] [get_bd_pins power_monitor_sys_rst/peripheral_aresetn]
         }
+
+        proc add_thermal_monitor {} {
+            # Add System Management and its system reset
+            create_bd_cell -type ip -vlnv xilinx.com:ip:system_management_wiz system_management
+            create_bd_cell -type ip -vlnv xilinx.com:ip:proc_sys_reset thermal_monitor_sys_rst
+
+            # Add an additional 100MHz clock for System Management
+            set num_out_clocks [get_property CONFIG.NUM_OUT_CLKS [get_bd_cells clock_generator]]
+            incr num_out_clocks
+            set_property -dict [list \
+              CONFIG.NUM_OUT_CLKS $num_out_clocks \
+              CONFIG.CLKOUT${num_out_clocks}_USED {true} \
+              CONFIG.CLKOUT${num_out_clocks}_REQUESTED_OUT_FREQ {100} \
+              CONFIG.CLK_OUT${num_out_clocks}_PORT {thermal_monitor_clk}
+            ] [get_bd_cells clock_generator]
+
+            # Connect System Management clock and reset
+            connect_clock [get_bd_pins thermal_monitor_sys_rst/slowest_sync_clk] [get_bd_pins clock_generator/thermal_monitor_clk]
+            connect_reset [get_bd_pins thermal_monitor_sys_rst/ext_reset_in] [get_bd_pins processor_system_reset/ext_reset_in]
+
+            # Connect System Management to the M_AXI interconnect
+            connect_to_axi_intf [get_bd_intf_pins system_management/S_AXI_LITE] M "" [get_bd_pins clock_generator/thermal_monitor_clk] [get_bd_pins thermal_monitor_sys_rst/peripheral_aresetn]
+        }
     }
 }
