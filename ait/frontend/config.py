d32ade481eb9f193d842b002a33d2984a1050e28
Miquel Vidal i Pinyol <miquel.vidal@bsc.es>
2024-04-12 17:03:18 +0200

    Bump version to 7.3.3

--- ait/frontend/config.py
+++ ait/frontend/config.py
@@ -25,7 +25,7 @@ MIN_WRAPPER_VERSION = 13
 BITINFO_VERSION = 11
 VERSION_MAJOR = 7
 VERSION_MINOR = 3
-VERSION_PATCH = 2
+VERSION_PATCH = 3
 
 # NOTE: The variable will be overwritten during installation, do not manually modify.
 VERSION_COMMIT = ''
