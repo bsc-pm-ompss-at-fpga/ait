0074ed5a1983a80d1f6e306881ba8d65da82ddba
Miquel Vidal i Pinyol <miquel.vidal@bsc.es>
2024-10-18 16:54:24 +0200

    Embed commit hash if COMMIT file is present

--- setup.py
+++ setup.py
@@ -33,6 +33,7 @@ class PostInstallCommand(install):
 
 tag = subprocess.run('git describe --tags --exact-match HEAD', shell=True, capture_output=True, encoding='utf-8').stdout.strip()
 commit_hash = subprocess.run('git show -s --format=%h', shell=True, capture_output=True, encoding='utf-8').stdout.strip()
+commit_file = subprocess.run('cat COMMIT', shell=True, capture_output=True, encoding='utf-8').stdout.strip()
 
 version_commit = ''
 if tag:
@@ -40,6 +41,8 @@ if tag:
 elif commit_hash:
     commit_hash += subprocess.run("git diff --quiet HEAD || echo '-dirty'", shell=True, capture_output=True, encoding='utf-8').stdout.strip()
     version_commit = 'commit: ' + commit_hash
+elif commit_file:
+    version_commit = 'commit: ' + commit_file
 
 if version_commit:
     with open('ait/frontend/config.py', 'r') as config_file:
