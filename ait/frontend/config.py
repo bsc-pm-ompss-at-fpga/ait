ba5f488d1993c821ffdbe8172c5ae96088a64103
Miquel Vidal i Pinyol <miquel.vidal@bsc.es>
2025-12-18 12:58:23 +0100

    Bump version to 8.1.0

--- ait/frontend/config.py
+++ ait/frontend/config.py
@@ -23,7 +23,7 @@ MIN_PYTHON_VERSION = (3, 8)
 MIN_WRAPPER_VERSION = 13
 BITINFO_VERSION = 15
 VERSION_MAJOR = 8
-VERSION_MINOR = 0
+VERSION_MINOR = 1
 VERSION_PATCH = 0
 
 # NOTE: The variable will be overwritten during installation, do not manually modify.
